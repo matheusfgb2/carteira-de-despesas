a642e23ef3248a3b47de0a078d514540d378fe1f
Matheus Boechat <matheusfgb@gmail.com>
2023-05-12 18:01:52 -0300

    requisito 2: criando header (com user email e total de despesas) para página wallet

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,40 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import logo from '../assets/logo.png';
+import './Header.css';
 
 class Header extends Component {
+  state = {
+    totalExpense: 0,
+    currency: 'BRL',
+  };
+
   render() {
+    const { totalExpense, currency } = this.state;
+    const { email } = this.props;
     return (
-      <div>Header</div>
+      <div className="header-container">
+        <img src={ logo } alt="Wallet" className="header-logo" />
+        <div className="user-info-container">
+          <p data-testid="email-field">{`Email: ${email}`}</p>
+          <p data-testid="total-field">
+            {`Despesa total: ${totalExpense},00 `}
+            <span data-testid="header-currency-field">{currency}</span>
+          </p>
+        </div>
+      </div>
     );
   }
 }
 
-export default Header;
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = ({ user }) => ({
+  email: user.email,
+});
+
+export default connect(mapStateToProps)(Header);
